c12195c1365449153a3a9273118b27b7e7860e9d
Jason He <zhouxiong.he@outlook.com>
2021-05-13 13:08:04 +0800

    add dollar2euro func

---
+++ 2.2.2-dollar2euro.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.2.2-dollar2euro) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require htdp/convert)
+
+(define (dollar->euro d)
+  (* 0.83 d)
+  )
