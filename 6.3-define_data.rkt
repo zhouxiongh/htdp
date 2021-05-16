79951d759933bf276e2ee2632f9597c57c3af2fc
hezhouxiong <hezhoux@qq.com>
2021-05-16 13:48:21 +0800

    Create 6.3-define_data.rkt
    data define

---
+++ 6.3-define_data.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 6.3-define_data) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 数据定义
+;; posn 是结构体
+;; (make-posn x y)
+;; 其中x和y是数
